706bb31c28842b9d28109a68b38f171cce5f6496
Arthur Tartaglia <arthur.t.pereira@edu.ufes.br>
2022-06-03 20:31:11 -0300

    Interação com o backend adicionada

--- src/main/java/com/example/arthur/tartaglia/Cliente.java
+++ src/main/java/com/example/arthur/tartaglia/Cliente.java
@@ -7,6 +7,10 @@ public class Cliente {
 
     }
 
+    public Cliente(String nome) {
+        this.nome = nome;
+    }
+
     public String getNome() {
         return nome;
     }
--- src/main/java/com/example/arthur/tartaglia/HelloWorldRest.java
+++ src/main/java/com/example/arthur/tartaglia/HelloWorldRest.java
@@ -4,20 +4,28 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.util.ArrayList;
+import java.util.List;
+
 @RestController
 public class HelloWorldRest {
     @GetMapping("/helloBuscar")
     public String buscar(@RequestParam(name = "fname") String nome) {
 
-        //IMPLEMENTAR AJAX PARA NAO TROCAR DE PAGINA AO CLICAR EM SUBMIT
-        //Tabela de clientes
-        String[] bancoClientes = {"Jose", "RodolFo", "LUIZ"};
+        //Simulando uma busca dos dados de um cliente em um banco de dados
+        List<Cliente> clientes = new ArrayList<>();
+        clientes.add(new Cliente("Jose"));
+        clientes.add(new Cliente("RodolFo"));
+        clientes.add(new Cliente("LUIZ"));
 
-        for (String cliente : bancoClientes) {
-            if (cliente.equalsIgnoreCase(nome)) {
-                return "Ok";
+        boolean retorno = false;
+        for (Cliente c : clientes) {
+            if (c.getNome().equalsIgnoreCase(nome)) {
+                retorno = true;
+                break;
             }
         }
-        return "Não";
+
+        return retorno? "Cliente encontrado!" : "Cliente NÃO encontrado!";
     }
 }

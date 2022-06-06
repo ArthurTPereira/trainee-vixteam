cfbb70996a941da4026558f7407073198665c0e6
Arthur Tartaglia <arthur.t.pereira@edu.ufes.br>
2022-06-06 19:57:43 -0300

    Adição da interação entre back-SQL
    Implementado a interação entre o backend e o servidor MySQL. Verificação se uma pessoa está no banco de dados concluída.

--- src/main/java/com/example/arthur/tartaglia/Cliente.java
+++ src/main/java/com/example/arthur/tartaglia/Cliente.java
@@ -1,6 +1,18 @@
 package com.example.arthur.tartaglia;
 
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name="cliente")
 public class Cliente {
+    @Id
+    @Column(name = "cliente_ID", nullable = false)
+    private long id;
+
+    @Column(name = "cliente_nome",nullable = false)
     private String nome;
 
     public Cliente() {
--- src/main/java/com/example/arthur/tartaglia/HelloWorldRest.java
+++ src/main/java/com/example/arthur/tartaglia/HelloWorldRest.java
@@ -4,28 +4,38 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.sql.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Objects;
 
 @RestController
 public class HelloWorldRest {
     @GetMapping("/helloBuscar")
     public String buscar(@RequestParam(name = "fname") String nome) {
 
-        //Simulando uma busca dos dados de um cliente em um banco de dados
-        List<Cliente> clientes = new ArrayList<>();
-        clientes.add(new Cliente("Jose"));
-        clientes.add(new Cliente("RodolFo"));
-        clientes.add(new Cliente("LUIZ"));
 
         boolean retorno = false;
-        for (Cliente c : clientes) {
-            if (c.getNome().equalsIgnoreCase(nome)) {
-                retorno = true;
-                break;
-            }
-        }
 
+        String sql = "SELECT cliente_nome from CLIENTE";
+        String url = "jdbc:mysql://localhost:3306/treinamento";
+        String user = "root";
+        String pwd = "123789cfm";
+
+        try (
+            Connection conn = DriverManager.getConnection(url,user,pwd);
+            Statement stmt = conn.createStatement();
+            ResultSet rs = stmt.executeQuery(sql);
+        ) {
+            while (rs.next()) {
+                if (rs.getString("cliente_nome").equalsIgnoreCase(nome)) {
+                    retorno = true;
+                    break;
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
         return retorno? "Cliente encontrado!" : "Cliente NÃO encontrado!";
     }
 }
